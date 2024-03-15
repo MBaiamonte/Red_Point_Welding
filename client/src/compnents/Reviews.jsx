55128be77a1c547f35939da092e41fe09f95d2df
MBaiamonte <120220332+MBaiamonte@users.noreply.github.com>
2024-03-14 21:00:04 -0600

    basic format

--- client/src/compnents/Reviews.jsx
+++ client/src/compnents/Reviews.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const Reviews = () => {
-  return (
-    <div>reviews</div>
-  )
+    return (
+        <div>
+            <h1>header</h1>
+        </div>
+    )
 }
 
 export default Reviews
