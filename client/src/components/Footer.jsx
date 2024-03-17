c6c182beb026562f95452ab480ba397d04ffd6b1
MBaiamonte <120220332+MBaiamonte@users.noreply.github.com>
2024-03-17 13:41:53 -0600

    add footer content

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,8 +2,23 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div>
-            <h1>footer</h1>
+        <div id='footer'>
+            <div className='d-flex flex-column justify-content-center align-items-center my-2'>
+                <h5>
+                Copyright 2024 <span>&copy;</span> Redpoint Welding, LLC
+                </h5>
+            </div>
+            <div className='d-flex justify-content-around'>
+                <div>
+                    <p>7614 Nikau Dr, Niwot, CO 80503</p>
+                </div>
+                <div>
+                    <h6> (303) 652-8222</h6>
+                </div>
+                <div>
+                    <p>Servicing Greater Colorado Front Range</p>
+                </div>
+            </div>
         </div>
     )
 }
