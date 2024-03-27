fff6ff9a3550394acc7e362b914b9e59ad239bf7
MBaiamonte <120220332+MBaiamonte@users.noreply.github.com>
2024-03-26 21:48:10 -0600

    add review

--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -132,6 +132,15 @@ const Reviews = () => {
                         <FontAwesomeIcon icon={faQuoteRight} className='ml-2' />
                     </p>
                 </div>
+                <div className='review'>
+                    <h3>Dan Bradley</h3>
+                    <p>
+                        <FontAwesomeIcon icon={faQuoteLeft} className='mr-2'/>
+                        Jim Hone, Owner of Redpoint Welding LLC, has been doing a welding project off and on regarding two overhead cranes for about a year.  
+                        He is reliable and does a good job with his welds.  He works well by himself or with others.  
+                        I would highly recommend Jim for any project that involves welding.                        <FontAwesomeIcon icon={faQuoteRight} className='ml-2' />
+                    </p>
+                </div>
             </Slider>
         </div>
     );
