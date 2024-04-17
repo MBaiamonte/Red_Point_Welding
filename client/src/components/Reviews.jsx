3057347d56e0f27e6ca7129d8c0af231c0b400d5
MBaiamonte <120220332+MBaiamonte@users.noreply.github.com>
2024-04-16 22:18:59 -0600

    update review

--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -18,14 +18,19 @@ const Reviews = () => {
 
     const Review = () =>[
         {
-            
+            name:"Pamela Stone",
+            content: "Of 5 different plumbers that I contacted, Jim was by far the most responsive. He was quick to come by my place to assess the issue with the hot water heater, and even delayed other projects as he was quick to determine which issues were considered more criticalHe worked tirelessly for 6 hours, well into the eve, to be sure everything was complete. The following day he came by again to check his work and make sure there was nothing amiss. His price was by far better than any of the others, and the work was perfect. I highly recommend him?
+    },
+    {
+        name:"",
+        content:""
     }
 ]
 
     return (
         <div id='slider'>
             <Slider {...settings}>
-                <div className='review'>
+                {/* <div className='review'>
                     <h3>Pamela Stone</h3>
                     <p>
                         <FontAwesomeIcon icon={faQuoteLeft} className='mr-2'/>
@@ -37,7 +42,7 @@ const Reviews = () => {
                         His price was by far better than any of the others, and the work was perfect. I highly recommend him! 
                         <FontAwesomeIcon icon={faQuoteRight} className='ml-2' />
                     </p>
-                </div>
+                </div> */}
                 <div className='review'>
                     <h3>Julia Rechter</h3>
                     <p>
