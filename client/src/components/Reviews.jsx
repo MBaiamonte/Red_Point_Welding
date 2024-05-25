7c3a64efa9157dec04f50171b727e4bfcd3a01a5
MBaiamonte <120220332+MBaiamonte@users.noreply.github.com>
2024-05-25 12:32:42 -0600

    clean up code

--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -373,14 +373,6 @@ const Reviews = () => {
                         <FontAwesomeIcon icon={faQuoteRight} className='ml-2' />
                     </p>
                 </div> */}
-                {/* <div className='review'>
-                    <h3>a v</h3>
-                    <p>
-                        <FontAwesomeIcon icon={faQuoteLeft} className='mr-2'/>
-                        a v Jim is an excellent pipe fitter and very knowledgeable, I have worked with him for years on several projects.
-                        <FontAwesomeIcon icon={faQuoteRight} className='ml-2' />
-                    </p>
-                </div> */}
             </Slider>
         </div>
     );
